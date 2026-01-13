3ad1d3c53b370ee8b17a472f656421fa8cef9ed7
yazzzti <yashtimittal123@gmail.com>
2026-01-13 15:11:00 +0530

    merge sort

---
+++ assignment_1/Q2_MergeSort.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void merge(vector<int> &nums, int low, int mid, int high){
+    int i,j;
+    vector<int> temp;
+    i=low;
+    j=mid+1;
+    while(i<=mid && j<=high){
+        if(nums[i]<nums[j]){
+            temp.push_back(nums[i]);
+            i++;
+        }
+        else{
+            temp.push_back(nums[j]);
+            j++;
+        }
+    }
+    while(i<=mid){
+        temp.push_back(nums[i]);
+        i++;
+    }
+    while(j<=high){
+        temp.push_back(nums[j]);
+        j++;
+    }
+    for(int k=0; k<temp.size(); k++){
+        nums[low+k] = temp[k];
+    }
+}
+
+void MergeSort(vector<int> &arr,int l, int h){
+    if(l<h){
+        int mid = (l+h)/2;
+        MergeSort(arr, l, mid);
+        MergeSort(arr,mid+1,h);
+
+        merge(arr,l,mid,h);
+    }
+}
+
+int main(){
+    vector<int> array = {23,45,32,1,2,10};
+    int n = array.size();
+    int low = 0;
+    int high = n-1;
+
+    MergeSort(array,low,high);
+    for(auto it:array){
+        cout<<it<<" ";
+    }
+}
